379d00014912d0e57bd8d9e9de7e26494d55facf
darowebdeveloper <darowebdeveloper@gmail.com>
2022-12-28 14:54:26 +0700

    prepare for heroku

--- server/server.js
+++ server/server.js
@@ -17,6 +17,16 @@ app.use('/api/exams', authMiddleware, examRoute);
 app.use('/api/reports', authMiddleware, reportRoute);
 
 const PORT = process.env.PORT || 5000;
+
+const path = require('path');
+__dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(PORT, () => {
   console.log('Server starting on port ' + PORT);
 });
